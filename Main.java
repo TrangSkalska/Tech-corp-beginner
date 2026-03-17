f20d1784308f48e976addb047f99287d33f6bec3
TrangSkalska <skalskathuytrang@gmail.com>
2026-03-17 09:51:49 +0000

    Add Main class with initial print statements for TechCorp simulator

---
+++ Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello from my TechCorp simulator project!");
+
+        System.out.println("This is the first line.");
+        System.out.println("This is the second line.");
+        System.out.println("This is the third line.");
+
+        System.out.println("Fictional company: NeonData Labs");
+    }
+}
